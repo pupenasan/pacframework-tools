ab901ea493e560082bad60cd418c12d37e6049d2
pupena_san <pupena_san@ukr.net>
2023-10-06 20:54:02 +0300

    1

--- test.js
+++ test.js
@@ -1,16 +1,2 @@
-//https://sebhastian.com/nodejs-download-file/
-const https = require('https'); // or 'https' for https:// URLs
-const fs = require('fs');
-
-const file = fs.createWriteStream("c:/temp/file.xlsx");
-const request = https.get(`https://docs.google.com/spreadsheets/uc?export=download&id=1GvttNOH74X2o9y0fh_qxQCHhfdFszx7m`, function(response) {
-   response.pipe(file);
-
-   // after download completed close filestream
-   file.on("finish", () => {
-       file.close();
-       console.log("Download Completed");
-   });
-});
-/*https://docs.google.com/spreadsheets/d/1GvttNOH74X2o9y0fh_qxQCHhfdFszx7m/edit?usp=sharing&ouid=111751208742846482260&rtpof=true&sd=true*/
-
+let DT1 = new Date("2020, 4, 18 19:29:33");
+console.log(DT1.toLocaleTimeString('uk-UA',{day:'numeric', month:'long', year:'numeric', weekday:'long', hour:'2-digit', minute:'2-digit', second: '2-digit'}));// субота, 18 квітня 2020 р., 19:29:33
